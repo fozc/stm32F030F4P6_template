ee3ae610d56b9f624dfdb9dcf51d571aae35c762
Fatih ÖZCAN <fatihozcan@gmail.com>
2021-11-10 14:42:29 +0300

    delete archive file

---
+++ main.h
@@ -0,0 +1,27 @@
+/**
+  ******************************************************************************
+  * @file    main.c 
+  * @author  Fatih OZCAN
+  * @version V1.0.0
+  * @date    28-Haziran-2016
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  */
+  
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx.h"
+ 
+ 
+
+
+ 
+	 
+ 
+#endif /* __MAIN_H */
+
+
---
+++ stm32f0xx_it.c
@@ -0,0 +1,127 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F0xx_StdPeriph_Templates/stm32f0xx_it.c 
+  * @author  MCD Application Team
+  * @version V1.5.0
+  * @date    05-December-2014
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides template for all exceptions handler and 
+  *          peripherals interrupt service routine.
+  ******************************************************************************
+  * @attention
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_it.h"
+ 
+ volatile uint32_t milli_sec = 0;
+
+ 
+/******************************************************************************/
+/*            Cortex-M0 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+	milli_sec++;
+}
+
+/******************************************************************************/
+/*            STM32F0xx Peripherals Interrupt Handlers                        */
+/******************************************************************************/
+/**
+* @brief  This function handles USART interrupt request.
+* @param  None
+* @retval None
+*/
+void USART1_IRQHandler(void)
+{
+
+}
+
+
+ 
+/**
+  * @brief  This function handles RTC Wakeup global interrupt request.
+  * @param  None
+  * @retval None
+  */
+void RTC_IRQHandler(void)
+{
+
+}
+
+
+
+
+
+
+
+
+/******************************************************************************/
+/*                 STM32F0xx Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f0xx.s).                                            */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles PPP interrupt request.
+  * @param  None
+  * @retval None
+  */
+/*void PPP_IRQHandler(void)
+{
+}*/
+
+/**
+  * @}
+  */ 
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
